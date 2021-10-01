f917ec1cac32af9897062fd35f9b0464568eccf7
tarekemad1 <tarekrayyan50@gmail.com>
2021-10-01 10:45:33 +0200

    updating

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+  int x;
+  printf("enter number:");
+  scanf("%d",&x);
+  printf("\n%d",x);
+  printf("\nx=%d",x<<4);
+
+  printf("\n%x",&x);
+
+    return 0;
+}
